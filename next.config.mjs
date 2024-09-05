f84c2a0f4dde3690341b2de60f9bcbbd30d1f38d
Shtekata <gesheval@gmail.com>
2024-09-05 09:05:17 +0300

    Add ppr

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+// const nextConfig = {}
+const nextConfig = { experimental: { ppr: 'incremental' } }
 
-export default nextConfig;
+export default nextConfig
