462c1d6d1373a2b8e00cefafe7b3d9f47544e46b
Shtekata <gesheval@gmail.com>
2024-10-06 20:10:59 +0300

    Adding form validation and improving accessibility

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,29 +6,57 @@ import { redirect } from 'next/navigation'
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({ invalid_type_error: 'Please select a customer.' }),
+  amount: z.coerce.number().gt(0, { message: 'Please enter an amount greater than 0 Lv.' }),
+  status: z.enum(['pending', 'paid'], { invalid_type_error: 'Please select an invoice status.' }),
   date: z.string(),
   time: z.string(),
 })
 
+export type State = {
+  errors?: {
+    customerId?: string[]
+    amount?: string[]
+    status?: string[]
+  }
+  message?: string | null
+}
+
 function validate(formData: FormData) {
   const ValidateInvoice = FormSchema.omit({ id: true, date: true, time: true })
   //   const rawFormData = Object.fromEntries(formData.entries())
-  const { customerId, amount, status } = ValidateInvoice.parse({
+  const validatedFields = ValidateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   })
-  const amountInCents = amount * 100
-  const date = new Date().toISOString().split('T')[0]
-  const time = new Date().toISOString().split('T')[1]
-  return { customerId, amount, status, amountInCents, date, time }
+
+  let customerId: string = ''
+  let amount: number = 0
+  let amountInCents: number = 0
+  let status: string = ''
+  let date: string = ''
+  let time: string = ''
+  let err: State = {}
+
+  if (validatedFields.success) {
+    ;({ customerId, amount, status } = validatedFields.data)
+    amountInCents = amount * 100
+    date = new Date().toISOString().split('T')[0]
+    time = new Date().toISOString().split('T')[1]
+  } else {
+    err = {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to create Invoice.',
+    }
+  }
+
+  return { customerId, status, amountInCents, date, time, err }
 }
 
-export async function createInvoice(formData: FormData) {
-  const { customerId, amountInCents, status, date, time } = validate(formData)
+export async function createInvoice(prevState: State, formData: FormData) {
+  const { customerId, status, amountInCents, date, time, err } = validate(formData)
+  if (err.errors) return err
 
   try {
     await sql`insert into invoices (customer_id, amount, status, date, time)
@@ -41,8 +69,9 @@ export async function createInvoice(formData: FormData) {
   redirect('/dashboard/invoices')
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amountInCents, status } = validate(formData)
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+  const { customerId, amountInCents, status, err } = validate(formData)
+  if (err.errors) return err
 
   try {
     await sql`update invoices set customer_id = ${customerId}, amount = ${amountInCents}, status = ${status} where id = ${id}`
