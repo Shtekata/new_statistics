eff909b6d18a84b17de768810fe1b8f06686ded2
Shtekata <gesheval@gmail.com>
2024-11-04 21:48:08 +0200

    Add Metadata object

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,26 @@
 import '@/app/ui/global.css'
 import { inter } from '@/app/ui/fonts'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'MyPerfume',
+  applicationName: 'MyPerfume',
+  description: 'Next.js application with many diverse function and capabilities!',
+  metadataBase: new URL('https://myperfume.bg'),
+  alternates: {
+    canonical: '/',
+    languages: {
+      'en-US': '/en-US',
+    },
+  },
+  openGraph: {
+    images: '/og-image.png',
+  },
+  creator: 'Asen Geshev',
+  publisher: 'Geshlandia Ltd.',
+  generator: 'Next.js',
+  keywords: ['Next.js', 'statistics', 'perfumes'],
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
