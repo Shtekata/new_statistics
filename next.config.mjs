93178fd828919f73aa7c9e3940366659f1e92be1
Shtekata <gesheval@gmail.com>
2024-11-05 21:32:54 +0200

    Add react-compiler

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 // const nextConfig = {}
-const nextConfig = { experimental: { ppr: 'incremental' } }
+const nextConfig = {
+  experimental: {
+    ppr: 'incremental',
+    reactCompiler: true,
+  },
+}
 
 export default nextConfig
